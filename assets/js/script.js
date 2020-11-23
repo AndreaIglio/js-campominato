f305c7090377982ac21d40a75c9226c29c3cf812
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-23 17:38:11 +0100

    Generate randomNumber not duplicate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,36 @@
-// Variabili con valore
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati
+
+var numberList = [];
+var i = 0;
+
+while (numberList.length < 16){
+
+var genNumber = Math.floor(Math.random() * 100 + 1);
+console.log(genNumber);
 
 
-// Variabili senza valore
+if (numberList.indexOf(genNumber) == -1) {
+
+numberList.push(genNumber);
+
+}
+ i++;
+
+}
+
+console.log(numberList);
+console.log(numberList.length);
+
+
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
