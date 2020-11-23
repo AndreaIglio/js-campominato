90e845bff2834ddb2ab0fd98b16311c441f0c8ff
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-23 18:38:57 +0100

    TASK 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,89 @@
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
+//TASK 1 Il computer deve generare 16 numeri casuali tra 1 e 100.
 // I numeri non possono essere duplicati
 
-var numberList = [];
+// function notDuplicate (array, number, iteration) {
+
+//        var i = 0;        
+
+//     while (array.length < iteration) {
+
+//         var number;
+
+//         if (array.indexOf(number) == -1) {
+//            return array.push(number);
+//         }
+
+//         i++
+
+//     }
+
+// }
+
+
+
+
+
+// var genNumber = Math.floor(Math.random() * 100 + 1);
+
+// var pcNumberFunction = notDuplicate (pcNumberList, genNumber, 16);
+// console.log(pcNumberFunction);
+
+
+
+
+var pcNumberList = [];
+
 var i = 0;
 
-while (numberList.length < 16){
+while (pcNumberList.length < 16){
 
 var genNumber = Math.floor(Math.random() * 100 + 1);
 console.log(genNumber);
 
 
-if (numberList.indexOf(genNumber) == -1) {
+if (pcNumberList.indexOf(genNumber) == -1) {
 
-numberList.push(genNumber);
+pcNumberList.push(genNumber);
 
 }
  i++;
 
 }
 
-console.log(numberList);
-console.log(numberList.length);
+console.log(pcNumberList);
 
 
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// TASK 2 In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+var userNumberList = [];
+
+while (userNumberList.length < 2 ){
+
+    var userNumber = Number(prompt('Inserisci un numero compreso tra 1 e 100'));
+
+    if(userNumberList.indexOf(userNumber) == -1) {
+
+        userNumberList.push(userNumber);
+
+    }
+
+    i++
+
+}
+
+console.log(userNumberList);
+
+
+
+//TASK 3 Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+
+
+
+
+//TASK 4 La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+//TASK 5 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 // BONUS: (da fare solo se funziona tutto il resto)
 // all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
 // con difficoltà 0 => tra 1 e 100
